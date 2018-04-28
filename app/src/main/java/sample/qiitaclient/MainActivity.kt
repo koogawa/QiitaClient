12218cd6d9391e2894e1f0fa9fcda697d8ba8010
Kosuke Ogawa <ogawa_kousuke@aratana.jp>
2018-04-29 03:18:00 +0900

    Update main activity

--- app/src/main/java/sample/qiitaclient/MainActivity.kt
+++ app/src/main/java/sample/qiitaclient/MainActivity.kt
@@ -2,11 +2,22 @@ package sample.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import sample.qiitaclient.model.Article
+import sample.qiitaclient.model.User
+import sample.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val articleView = ArticleView(applicationContext)
+
+        articleView.setArticle(Article("123",
+                "Kotlin Start",
+                "http://www.hoge.kasu",
+                User("456", "Taro", "")))
+
+        setContentView(articleView)
     }
 }
