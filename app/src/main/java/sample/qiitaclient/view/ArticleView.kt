aaf0625dc428e4e51d6cfa62698eb0528d88b075
Kosuke Ogawa <ogawa_kousuke@aratana.jp>
2018-05-02 01:25:43 +0900

    Use data binding for view_article

--- app/src/main/java/sample/qiitaclient/view/ArticleView.kt
+++ app/src/main/java/sample/qiitaclient/view/ArticleView.kt
@@ -1,6 +1,9 @@
 package sample.qiitaclient.view
 
 import android.content.Context
+import android.databinding.BindingMethod
+import android.databinding.BindingMethods
+import android.databinding.DataBindingUtil
 import android.graphics.Color
 import android.support.annotation.StyleRes
 import android.util.AttributeSet
@@ -11,6 +14,7 @@ import android.widget.TextView
 import com.bumptech.glide.Glide
 import sample.qiitaclient.R
 import sample.qiitaclient.bindView
+import sample.qiitaclient.databinding.ViewArticleBinding
 import sample.qiitaclient.model.Article
 
 class ArticleView : FrameLayout {
@@ -28,20 +32,25 @@ class ArticleView : FrameLayout {
                 defStyleAttr: Int,
                 defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
-    private val profileImageView: ImageView by bindView(R.id.profile_image_view)
+//    private val profileImageView: ImageView by bindView(R.id.profile_image_view)
+//
+//    private val titleTextView: TextView by bindView(R.id.title_text_view)
+//
+//    private val userNameTextView: TextView by bindView(R.id.user_name_text_view)
 
-    private val titleTextView: TextView by bindView(R.id.title_text_view)
-
-    private val userNameTextView: TextView by bindView(R.id.user_name_text_view)
+    val binding: ViewArticleBinding
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.view_article, this)
+//        LayoutInflater.from(context).inflate(R.layout.view_article, this)
+        binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.view_article, this, true)
     }
 
+
     fun setArticle(article: Article) {
-        titleTextView.text = article.title
-        userNameTextView.text = article.user.name
-        Glide.with(context).load(article.user.profileImageUrl).into(profileImageView)
+        binding.article = article
+//        titleTextView.text = article.title
+//        userNameTextView.text = article.user.name
+//        Glide.with(context).load(article.user.profileImageUrl).into(profileImageView)
         // todo プロフせっと
 //        profileImageView?.setBackgroundColor(Color.RED)
     }
